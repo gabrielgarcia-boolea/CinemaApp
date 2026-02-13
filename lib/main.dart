dd82459f0e7defa3db1112ca5abb6234db3d9d8d
Gabriel García Martínez <ggarcia@boolea.es>
2026-02-13 10:30:26 +0100

    Inicio

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:cinema_app/config/router/app_router.dart';
+import 'package:cinema_app/config/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      title: 'Flutter Demo',
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
